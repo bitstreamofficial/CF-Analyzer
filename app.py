4e59fca86cfa4baeb6d122afb0d2ce72a8a74d6b
BitStream <official.bitstream@gmail.com>
2025-05-20 23:58:30 +0600

    Update app.py

--- app.py
+++ app.py
@@ -476,4 +476,4 @@ def get_heatmap_data(handle, year):
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(host='0.0.0.0',debug=True) 
